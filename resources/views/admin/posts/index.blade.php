c2b27ae393269f578c152441b442f1972504c26d
unknown <wbhaese@gmail.com>
2021-06-01 20:19:14 -0300

    Add new style

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -5,13 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" type="text/css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
     <title>View Posts</title>
 
 </head>
 <body>
-    <button>
-        <a href="{{route('posts.create')}}">Create new Post</a>
-    </button>
+    
     <hr>
 
     @if(session('message'))
@@ -21,8 +24,8 @@
 
     <form action="{{route('posts.search')}}" method="post">
         @csrf
-        <input type="text" name="search" placeholder="Example: Post123...">
-        <button type="submit">Filter</button>
+        <input class="search-input" type="text" name="search" placeholder="Example: Post123...">
+        <button type="submit" class="btn btn-success">Filter</button>
     </form>
 
     <h1>Post Index</h1>
@@ -30,18 +33,37 @@
     <div class="posts-view">
         @foreach ($posts as $post)
             <div class="single-post">
-                <img src="{{url("storage/{$post->image}")}}" alt="{{$post->image}}" style="max-width: 100px">
-                Title: 
-                <h2>{{$post->title}}</h2>
-                    Description: <strong>{{$post->content}}</strong>
+                <div class="top-info">
+                    <div>
+                        <img src="{{url("storage/{$post->image}")}}" alt="{{$post->image}}" style="max-width: 100px">
+                    </div>
+                    <div class="w-75 p-3 index-title">
+                        <h3>Title:</h3>                    
+                        <h2> {{$post->title}}</h2>
+                    </div>
+                </div>
+                
+                Description: <strong>{{$post->content}}</strong>
                 <br/>
                 <div class="details-div">
-                    <a href="{{route('posts.show', $post->id )}}">Details</a>
-                    <a href="{{route('posts.edit', $post->id )}}">Edit</a>
+                   
+                    
+                    <div class='btn btn-primary'>
+                        <a class="pages-link" href="{{route('posts.show', $post->id )}}">Details</a>
+                    </div>
+                    <div class='btn btn-warning'>
+                        <a class="pages-link" href="{{route('posts.edit', $post->id )}}">Edit</a>
+                    </div>
+                    
                 </div>
             </div>
         @endforeach
     </div>
+    <div class="create-post">
+        <button class="btn btn-primary" id="create-btn">
+            <a class="pages-link" href="{{route('posts.create')}}">Create new Post</a>
+        </button>
+    </div>   
 
     <hr>
 
